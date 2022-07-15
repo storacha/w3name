0a630edc656ddfa68cdfe276469dc1c20842bc0f
Francois <francois@potatolondon.com>
2022-07-15 16:42:03 +0100

    Setup and handle alarm

--- packages/api/src/record.ts
+++ packages/api/src/record.ts
@@ -1,5 +1,6 @@
 import type { Env } from './env'
 import { jsonResponse } from './utils/json-response'
+import fetch from '@web-std/fetch'
 
 export interface IPNSRecordData {
   key: string
@@ -8,6 +9,9 @@ export interface IPNSRecordData {
   seqno: string
   validity: string
 }
+
+const REBROADCAST_INTERVAL_MS = 12 * 60 * 60 * 1000
+const BROADCAST_ENDPOINT = 'https://ipns-publisher.w3name.com'
 
 export function canOverwrite (current: IPNSRecordData, candidate: IPNSRecordData): boolean {
   // Logic copied from https://github.com/ipfs/go-ipns/blob/a8379aa25ef287ffab7c5b89bfaad622da7e976d/ipns.go#L325
@@ -48,6 +52,8 @@ export class IPNSRecord {
   }
 
   async fetch (request: Request) {
+    // Make sure we set an alarm for the next rebroadcast
+    const currentAlarm = await this.state.storage.getAlarm()
     const map: Map<string, any> = await this.state.storage.get(['key', 'record', 'hasV2Sig', 'seqno', 'validity'])
 
     // Values will be set to undefined if the record was not created prior to fetching it
@@ -59,7 +65,11 @@ export class IPNSRecord {
       validity: map.get('validity')
     }
 
-    if (request.method === 'PUT') {
+    if (currentAlarm === null) {
+      await this.state.storage.setAlarm(Date.now() + REBROADCAST_INTERVAL_MS)
+    }
+
+    if (request.method === 'PUT') { 
       const payload: IPNSRecordData = await request.json()
       // Setting undefined values as default if not in the payload
       const data = {
@@ -69,16 +79,49 @@ export class IPNSRecord {
         seqno: payload.seqno,
         validity: payload.validity
       }
-
+      
       if (record.key !== undefined && !canOverwrite(record, data)) {
         return jsonResponse(JSON.stringify({ message: 'invalid record: the record is outdated.' }), 400)
       }
-
+      
       await this.state.storage.put(data)
 
       return jsonResponse(JSON.stringify(data), 200)
     }
 
-    return jsonResponse(JSON.stringify(record), 200)
+    const data = this.getIPNSRecordData()
+
+    return jsonResponse(JSON.stringify(data), 200)
+  }
+
+  async alarm () {
+    const now = new Date()
+    await this.state.storage.setAlarm(Date.now() + REBROADCAST_INTERVAL_MS)
+
+    const data = await this.getIPNSRecordData()
+
+    const response = await fetch(BROADCAST_ENDPOINT, {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+
+    if (response.ok === true) {
+      await this.state.storage.put('lastRebroadcast', now.toISOString())
+    }
+  }
+
+  async getIPNSRecordData (): Promise<IPNSRecordData> {
+    const map: Map<string, any> = await this.state.storage.get(['key', 'record', 'hasV2Sig', 'seqno', 'validity'])
+
+    // Values will be set to undefined if the record was not created prior to fetching it
+    const data: IPNSRecordData = {
+      key: map.get('key'),
+      record: map.get('record'),
+      hasV2Sig: map.get('hasV2Sig'),
+      seqno: map.get('seqno'),
+      validity: map.get('validity')
+    }
+
+    return data
   }
 }
