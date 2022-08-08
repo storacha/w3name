ec35a0eb09a2f5e76db45ba55f157030c5729081
Adam Alton <270255+adamalton@users.noreply.github.com>
2022-08-08 15:03:53 +0100

    feat: add CORS headers to responses and an OPTIONS handler

---
+++ packages/api/src/cors.ts
@@ -0,0 +1,66 @@
+/* eslint-env serviceworker */
+import type { RouteHandler } from 'itty-router'
+
+/**
+ * Handler for CORS OPTIONS requests.
+ */
+export function corsOptions(request: Request): Response {
+  const headers = request.headers
+  // Make sure the necessary headers are present for this to be a valid pre-flight request
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    /** @type {Record<string, string>} */
+    const respHeaders = {
+      'Content-Length': '0',
+      'Access-Control-Allow-Origin': headers.get('origin') || '*',
+      'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+      'Access-Control-Max-Age': '86400',
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      'Access-Control-Allow-Headers':
+        headers.get('Access-Control-Request-Headers') || '',
+      'Access-Control-Expose-Headers': 'Link, Count, Page, Size'
+    }
+
+    return new Response(null, {
+      status: 204,
+      headers: respHeaders
+    })
+  } else {
+    return new Response('Non CORS options request not allowed', {
+      status: 405,
+      statusText: 'Method Not Allowed'
+    })
+  }
+}
+
+/**
+ * Wraps a handler which adds CORS headers to the responses.
+ */
+export function withCorsHeaders(handler: RouteHandler<Request>): RouteHandler<Request> {
+  return async (request: Request, ...rest: any[]): Promise<Response> => {
+    const response = await handler(request, ...rest)
+    return addCorsHeaders(request, response)
+  }
+}
+
+/**
+ * Adds CORS headers to an already-generated response.
+ */
+export function addCorsHeaders(request: Request, response: Response): Response {
+  // Clone the response so that it's no longer immutable (like if it comes from cache or fetch)
+  response = new Response(response.body, response)
+  const origin = request.headers.get('origin')
+  if (origin) {
+    response.headers.set('Access-Control-Allow-Origin', origin)
+    response.headers.set('Vary', 'Origin')
+  } else {
+    response.headers.set('Access-Control-Allow-Origin', '*')
+  }
+  response.headers.set('Access-Control-Expose-Headers', 'Link, Count, Page, Size')
+  return response
+}
---
+++ packages/api/test/cors.spec.ts
@@ -0,0 +1,68 @@
+/* eslint-env mocha */
+import assert from 'assert'
+import { Miniflare, Request } from 'miniflare'
+import { endpoint } from './scripts/constants.js'
+
+let mf: Miniflare
+
+before((done) => {
+  mf = new Miniflare({
+    envPath: true,
+    packagePath: true,
+    wranglerConfigPath: true,
+    wranglerConfigEnv: 'test',
+    modules: true,
+    bindings: {
+      REBROADCAST_INTERVAL_MS: 0
+    }
+  })
+  done()
+})
+
+describe('CORS', () => {
+
+  it('sets CORS headers on 404', async () => {
+    // const res = await fetch(new URL('nope', endpoint))
+    const res = await mf.dispatchFetch(new URL(`imaginary`, endpoint))
+    assert(!res.ok)
+    assert.strictEqual(res.status, 404, 'Expected 404 on /nope')
+    assert.strictEqual(res.headers.get('Access-Control-Allow-Origin'), '*')
+  })
+
+  it('sets CORS headers on server error', async () => {
+    // const res = await fetch(new URL('error', endpoint))
+    const path = '/name/invalidkey'
+    const res = await mf.dispatchFetch(new URL(path, endpoint))
+    assert(!res.ok)
+    assert.strictEqual(res.status, 400, `Expected 400 on ${path}`)
+    assert.strictEqual(res.headers.get('Access-Control-Allow-Origin'), '*')
+  })
+
+  it('correctly responds to preflight request', async () => {
+    const res = await mf.dispatchFetch(
+      new Request(
+        new URL(`name/abcd`, endpoint),
+        {
+          method: 'OPTIONS',
+          headers: {
+            Origin: 'name.web3.storage',
+            'Access-Control-Request-Method': 'whatever',
+            'Access-Control-Request-Headers': 'whatever'
+          }
+        }
+      )
+    )
+    // const res = await fetch(new URL('/name/abcd', endpoint), {
+    //   method: 'OPTIONS',
+    //   headers: {
+    //     Origin: 'name.web3.storage',
+    //     'Access-Control-Request-Method': 'whatever',
+    //     'Access-Control-Request-Headers': 'whatever'
+    //   }
+    // })
+    assert(res.ok)
+    assert.strictEqual(res.status, 204, 'Expected 204 status for OPTIONS request')
+    assert.strictEqual(res.headers.get('Access-Control-Allow-Origin'), 'name.web3.storage')
+    assert.strictEqual(res.headers.get('Access-Control-Allow-Methods'), 'GET,POST,DELETE,OPTIONS')
+  })
+})
