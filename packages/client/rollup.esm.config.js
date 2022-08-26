1c71b7f5a887283bd3e5910e6ff220bf37918fa3
Francois <francois@potatolondon.com>
2022-08-26 12:26:53 +0100

    Update config

--- packages/client/rollup.esm.config.js
+++ packages/client/rollup.esm.config.js
@@ -1,31 +1,21 @@
 // @ts-ignore
 import { terser } from 'rollup-plugin-terser'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import json from '@rollup/plugin-json'
 import typescript from '@rollup/plugin-typescript'
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      // inlineDynamicImports: true,
-      // file: 'dist/bundle.esm.min.js',
       dir: 'dist',
       preserveModules: true,
       format: 'esm',
       entryFileNames: '[name].mjs',
-      // plugins: [terser()],
+      plugins: [terser()],
       sourcemap: true
     }
   ],
   plugins: [
-    // commonjs(),
-    // resolve({
-    //   preferBuiltins: false,
-    //   browser: true
-    // }),
-    // json(),
     typescript()
   ]
 }
+
